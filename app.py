faa6a5cac4d9eb90e872d86d0d93edc55b8d80d8
Victor Cuartas <vicuartas230@gmail.com>
2024-08-19 12:26:44 -0500

    Finish edit route and fix errors in add route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from models.storage import Storage
 from models.device import Device
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -11,37 +12,47 @@ storage.load_session()
 
 @app.route("/", strict_slashes=False)
 def index():
-    return render_template('index.html', items=storage.all())
+    return render_template("index.html", items=storage.all())
 
-@app.route("/add", methods=['POST', 'GET'], strict_slashes=False)
+@app.route("/add", methods=["POST", "GET"], strict_slashes=False)
 def create():
-    if request.method == 'POST':
+    if request.method == "POST":
         fields = [
-            "name"
-            "price"
-            "mac_address"
-            "serial_number"
-            "manufacturer"
-            "description"
+            "name",
+            "price",
+            "mac_address",
+            "serial_number",
+            "manufacturer",
+            "description",
         ]
         new_item = {
-            field.replace('_', '-'): (float(request.form[field])
-                                      if field == "price"
-                                      else request.form[field])
-                                      for field in fields
+            field:
+            request.form[field.replace('_', '-')]
+             for field in fields
         }
         new_obj = Device(**new_item)
         storage.new(new_obj)
         storage.save()
-    return render_template('add.html')
+        return redirect(url_for("index"))
+    return render_template("add.html")
 
-@app.route("/edit/<int:id>", methods=['GET', 'PUT'], strict_slashes=False)
+@app.route("/edit/<int:id>", methods=["GET", "POST"], strict_slashes=False)
 def edit(id):
     el = storage.get(id)
-    if el is not None:
-        for k, v in el.items():
-            print("key: ", k, "Value: ", v)
-    return render_template('edit.html')
+    if request.form.get("_method") == "PUT":
+        not_in_form = ["_sa_instance_state", "id", "created_at", "updated_at"]
+        edited = False
+        for key, value in el.__dict__.items():
+            if key not in not_in_form:
+                form_value = request.form[key.replace('_', '-')]
+                if form_value != value:
+                    setattr(el, key, form_value)
+                    edited = True
+        if edited:
+            el.updated_at = datetime.now()
+            storage.save()
+        return redirect(url_for("index"))
+    return render_template("edit.html", el=el)
 
 @app.route("/delete/<int:id>", strict_slashes=False)
 def delete(id):
@@ -53,6 +64,4 @@ def delete(id):
     else:
         storage.delete(el)
         storage.save()
-        return {
-            "result": "done"
-        }
+        return redirect(url_for("index"))
--- models/device.py
+++ models/device.py
@@ -10,9 +10,9 @@ class Device(Base):
     __tablename__ = 'inventory'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(255), nullable=False)
-    price = Column(Float, nullable=False)
-    mac_address = Column(String(32))
-    serial_number = Column(String(64))
+    price = Column(String(16), nullable=False)
+    mac_address = Column(String(32), unique=True)
+    serial_number = Column(String(64), unique=True)
     manufacturer = Column(String(255))
     description = Column(String())
     created_at = Column(DateTime, default=datetime.now())
